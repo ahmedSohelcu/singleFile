b17c2e4042790517c9f144a7c7a22e7dc017cc7c
ahmedSohelcu <ahmedsohelcu@gmail.com>
2019-02-27 10:19:50 +0600

    same for and validation msg

---
+++ 09.curd same form .blade.php
@@ -0,0 +1,32 @@
+1.{{--for add page --}}
+{{ Form::open(['route'=>'save.shift_group','method'=>'POST', 'class'=>'form-horizontal']) }}
+@include('hrm.settings.shiftGroup.form-shift-group',['buttonText'=>'save'])
+{{ Form::close() }}
+
+    OR
+
+{{ Form::open(['action'=>'UserController@store','method'=>'post']) }}
+    @include('user.form')
+{{ Form::close() }}
+---------------------------------------------------------------------------------------------------------------
+2.{{--for edit page--}}
+{{ Form::model($shiftGroup,['route'=>['update.shift_group',$shiftGroup->id],'method'=>'patch','class'=>'form-horizontal']) }}
+    @include('hrm.settings.shiftGroup.form-shift-group',['buttonText'=>'Update'])
+{{ Form::close() }}
+
+    OR
+
+{{ Form::model($user,['action'=>['UserController@update',$user->id],'method'=>'patch']) }}
+{{ Form::close() }}
+---------------------------------------------------------------------------------------------------------------
+3.{{--for delete page--}}
+<td>
+    {{ Form::open(['action'=>['Hrm\ShiftController@destroyShiftGroup',$shiftGroup->id],'method'=>'delete','onsubmit'=>'return confirmDelete()']) }}    &nbsp;&nbsp;&nbsp;
+    <a href="{{ action('Hrm\ShiftController@editShiftGroup',$shiftGroup->id) }}" type="button" class="btn btn-info btn-sm">
+        <i class="fa fa-pencil-alt"></i>
+    </a>    &nbsp;&nbsp;
+    <button type="submit" class="btn btn-danger btn-sm">
+        <i class="fa fas fa-trash"></i>
+    </button>
+    {{ Form::close() }}
+</td>
