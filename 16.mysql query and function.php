d7632b03fd51b6633bde3ca09a921dc1e60fb3c9
Mohammed Ahmed ullah <ahmedsohelcu@gmail.com>
2019-03-25 10:40:30 +0600

    export mqsql file

---
+++ 16.mysql query and function.php
@@ -0,0 +1,8 @@
+//To Export big mysql table file using command line
+===============================================================
+
+        **1   F:\xampp\mysql\bin>mysqldump -u root wf floor>D:/floorNewName.sql
+                    1. wf    -> database name
+                    2. floor -> our table name which we want to export
+                    3. D:    -> driver where we want to save exported table
+                    4. floorNewName    ->exported file will save as floorNewName
