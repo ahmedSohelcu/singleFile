18ae721af2d73142d70777a42563c07ef9a855c2
Mohammed Ahmed ullah <ahmedsohelcu@gmail.com>
2019-03-25 16:56:24 +0600

    update

--- 17.search data according to 4 criteria from laravel  same file and method.blade.php
+++ 17.search data according to 4 criteria from laravel  same file and method.blade.php
@@ -23,3 +23,44 @@
 
         return view('hrm.information.all-employees',compact('employees'));
     }
+	
+	
+	
+	//view 
+	  {{--start search Employee Information --}}
+    <section id="employeeInfoSearch">
+        <div class="container col-md-12">
+            {{ Form::open(['action'=>'Hrm\HrmInformationController@view','method'=>'get']) }}
+                <div class="form-row">
+                    <div class="form-group col-md-2">
+                        <label for="employee_no">Employee No</label>
+                        <input type="text" class="form-control" id="employee_no" name="employee_no">
+                    </div>
+
+                    <div class="form-group col-md-2">
+                        <label for="card_no">Card No</label>
+                        <input type="text" class="form-control" id="card_no" name="card_no">
+                    </div>
+
+                    <div class="form-group col-md-2">
+                        <label for="status">Active or Inactive</label>
+                        <select id="status" name="status" class="form-control">
+                            <option selected value="">Choose...</option>
+                            <option value="active">Active</option>
+                            <option value="inactive">Inactive</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-md-2">
+                        <label for="bname">Name in Bengali</label>
+                        <input type="text" class="form-control" id="bname" name="bname">
+                    </div>
+
+                    <div class="form-group col-md-1"><br>
+                        <button type="submit" class="btn btn-primary">Search </button>
+                    </div>
+                </div>
+            {{ Form::close() }}
+        </div>
+    </section>
+    {{--end search Employee Information --}}
