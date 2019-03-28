713fb6ee15331fdcfc1439c78eb1e8659b2aefd1
Mohammed Ahmed ullah <ahmedsohelcu@gmail.com>
2019-03-28 17:18:36 +0600

    add query

--- 15.Eloquent query.blade.php
+++ 15.Eloquent query.blade.php
@@ -10,9 +10,6 @@ $totalEmp   = Attendance::all()->count();
 //getting present employee number
 $presentEmp = Attendance::query()->where('status','p')->get()->count();
 
-//getting total lated
-$totalLated = Attendance::query()->whereTime('in_time', '>', Carbon::parse('09:00'))->get()->count();
-
 $designations = Designation::all()->pluck('name','id');
 
 user = User::query()->findOrFail($id);
@@ -21,5 +18,61 @@ $roles = Role::all()->pluck('name','id');
 
 Unit::all()->pluck('name','id');
 
+//getting total lated
+$totalLated = Attendance::query()->whereTime('in_time', '>', Carbon::parse('09:00'))->get()->count();
+
+$results = User::where('this', '=', 1)
+    ->where('that', '=', 1)
+    ->where('this_too', '=', 1)
+    ->where('that_too', '=', 1)
+    ->where('this_as_well', '=', 1)
+    ->where('that_as_well', '=', 1)
+    ->where('this_one_too', '=', 1)
+    ->where('that_one_too', '=', 1)
+    ->where('this_one_as_well', '=', 1)
+    ->where('that_one_as_well', '=', 1)
+    ->get();
+	
+	OR
+	
+	$query->where([
+		['column_1', '=', 'value_1'],
+		['column_2', '<>', 'value_2'],
+		[COLUMN, OPERATOR, VALUE],
+		...
+	]);	
+		
+	
+	User::where('this', '=', 1)
+		->whereNotNull('created_at')
+		->whereNotNull('updated_at')
+		->where(function($query){
+			return $query
+			->whereNull('alias')
+			->orWhere('alias', '=', 'admin');
+		});
+
+
+	$attendances = \App\Attendance::whereNotNull('employee_id')
+				->whereNull('out_time')
+				->whereSection_id($section->id)
+				->where('date',\Carbon\Carbon::yesterday())
+				->get();				
+
+
+
 {{ Form::select('unit_id',$repository->units(),null,['class'=>'form-control','id'=>"unit_id", 'placeholder'=>'select unit'])}}
+
+
+
+
+
+
+
+
+where('that', 1)
+where('that', '=', 1)
+
+
+
 
