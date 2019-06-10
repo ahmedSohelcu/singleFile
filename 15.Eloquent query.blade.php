030a398e91bc518b58d409382b619be81720822b
Mohammed Ahmed ullah <ahmedsohelcu@gmail.com>
2019-06-10 12:11:42 +0600

    add more query

--- 15.Eloquent query.blade.php
+++ 15.Eloquent query.blade.php
@@ -21,6 +21,10 @@ Unit::all()->pluck('name','id');
 //getting total lated
 $totalLated = Attendance::query()->whereTime('in_time', '>', Carbon::parse('09:00'))->get()->count();
 
+$efg = Employee::query()->where('status',0)->whereHas('office',function($query){
+	$query->groupBy('section_id');
+})->first();
+
 $results = User::where('this', '=', 1)
     ->where('that', '=', 1)
     ->where('this_too', '=', 1)
@@ -57,21 +61,36 @@ $results = User::where('this', '=', 1)
 				->whereNull('out_time')
 				->whereSection_id($section->id)
 				->where('date',\Carbon\Carbon::yesterday())
-				->get();				
+				->get();
+
+{{--//==============================================================================================--}}
+	if(Input::has('start') && Input::has('end')){
+		$start = Carbon::parse(Input::get('start'))->startOfDay();
+		$end = Carbon::parse(Input::get('end'))->endOfDay();
+
+		$start = $start->format('Y-m-d');
+		$end =  $end->format('Y-m-d');
+		//dd($start,$end);
+
+		$abc = EmployeeOffice::query();
+
+		if(Auth::user()->unit_id){
+			$abc->where('unit_id',Auth::user()->unit_id);
+		}else{
+			$abc->whereIn('unit_id',[8,10,11]);
+		}
+
+		$abc = $abc->whereHas('employee',function ($query) use ($start,$end) {
+			$query->whereBetween('inactivedate',[$start, $end])->where('status',0);
+		})->paginate()->groupBy('section_id');
+		}else{
+			$abc = [];
+			$start = '';
+			$end = '';
+		}
+{{--//==============================================================================================--}}
 
 
-
-{{ Form::select('unit_id',$repository->units(),null,['class'=>'form-control','id'=>"unit_id", 'placeholder'=>'select unit'])}}
-
-
-
-
-
-
-
-
-where('that', 1)
-where('that', '=', 1)
 
 
 
