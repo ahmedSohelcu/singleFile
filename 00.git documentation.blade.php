741dbbe2e340623e22d01766085cb2f5a63a8ebb
Mohammed Ahmed ullah <ahmedsohelcu@gmail.com>
2019-10-12 10:06:48 +0600

    git documentation

---
+++ 00.git documentation.blade.php
@@ -0,0 +1,26 @@
+1. Check your information
+---------------------------------------------
+git config --global --list
+
+2.Add your Git username and set your email
+---------------------------------------------
+git config --global user.name "YOUR_USERNAME"
+
+3. Then verify that you have the correct username:
+---------------------------------------------
+git config --global user.name
+
+
+4.To set your email address, type the following command:
+---------------------------------------------
+git config --global user.email "your_email_address@example.com"
+
+
+5.To verify that you entered your email correctly, type:
+---------------------------------------------
+git config --global user.email
+
+
+6. Check your information
+---------------------------------------------
+git config --global --list
