fa90e362abbd0668327dfd7847f207280473c90f
Mohammed Ahmed ullah <ahmedsohelcu@gmail.com>
2019-10-13 17:05:13 +0600

    list of git command

---
+++ 000.Git command.blade.php
@@ -0,0 +1,18 @@
+============================================================================================
+							Essential Git Commands
+============================================================================================
+1.git init
+
+2.git config --global user.name "name here"
+
+2.git config --global user.email "email here"
+
+private
+--------------------------------------------
+3.git config user.name "name here"
+
+4.git config user.email "email here"
+
+5.git config --list
+
+
