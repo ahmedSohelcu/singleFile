9ee02240cb2c268e7d0a7af174ca844a8a60012e
Mohammed Ahmed ullah <ahmedsohelcu@gmail.com>
2019-10-13 15:27:07 +0600

    react native command

--- 27.laravel mysql generate next auto increment number.blade.php
+++
@@ -1,6 +0,0 @@
-========================================================================================
-Laravel MYSQL generate next auto-increment id 
-========================================================================================
-$query = DB::select(DB::Raw("SHOW TABLE STATUS LIKE 'leaves'"));
-        $query= $query[0]->Auto_increment;
-        dd($query);
