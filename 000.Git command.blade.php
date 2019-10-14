911688f18ab157f81ce8ae920c3ecbc0ec301ef8
Ahmed ullah <46097797+ahmedSohelcu@users.noreply.github.com>
2019-10-14 11:15:51 +0600

    Update 000.Git command.blade.php
    create a new repository  or push an existing repository from the command line

--- 000.Git command.blade.php
+++ 000.Git command.blade.php
@@ -15,4 +15,21 @@ private
 
 5.git config --list
 
+--------------------------------------------
+…create a new repository on the command line
+--------------------------------------------
+echo "# nativePro" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/ahmedSohelcu/nativePro.git
+git push -u origin master
+------------------------------------------------------
 
+
+------------------------------------------------------
+… OR push an existing repository from the command line
+------------------------------------------------------
+git remote add origin https://github.com/ahmedSohelcu/nativePro.git
+git push -u origin master
+------------------------------------------------------
