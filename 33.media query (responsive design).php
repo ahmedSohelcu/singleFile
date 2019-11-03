d403e163849df9bad3aeb93eb0b505cf1ea68497
Mohammed Ahmed ullah <ahmedsohelcu@gmail.com>
2019-11-03 11:01:43 +0600

    media query/break point

---
+++ 33.media query (responsive design).php
@@ -0,0 +1,40 @@
+
+====================================================================
+Media Query....
+====================================================================
+Having all CSS for desktop on style.css
+All media queries on responsive.css: all CSS for responsive menu + media break points
+-------------------------------------------------------------------
+From stack overflow + my combination
+-------------------------------------------------------------------
+@media only screen and (min-width: 320px) and (max-width: 479px){ ... }
+
+@media only screen and (min-width: 480px) and (max-width: 767px){ ... }
+
+@media only screen and (min-width: 768px) and (max-width: 991px){ ... }
+
+/* Large devices (laptops/desktops, 992px and up) */
+@media only screen and (min-width: 992px){ ... }
+
+/* Extra large devices (large laptops and desktops, 1200px and up) */
+@media only screen and (min-width: 1200px) {...} 
+
+
+
+-------------------------------
+From w3cschool
+--------------------------------
+ /* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 600px) {...}
+
+/* Small devices (portrait tablets and large phones, 600px and up) */
+@media only screen and (min-width: 600px) {...}
+
+/* Medium devices (landscape tablets, 768px and up) */
+@media only screen and (min-width: 768px) {...}
+
+/* Large devices (laptops/desktops, 992px and up) */
+@media only screen and (min-width: 992px) {...}
+
+/* Extra large devices (large laptops and desktops, 1200px and up) */
+@media only screen and (min-width: 1200px) {...} 
