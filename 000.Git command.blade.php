d752c1522168e0dfbe8e2a329b0597b5d64ff935
Ahmed ullah <46097797+ahmedSohelcu@users.noreply.github.com>
2022-06-24 10:16:56 +0600

    Update 000.Git command.blade.php

--- 000.Git command.blade.php
+++ 000.Git command.blade.php
@@ -33,3 +33,31 @@ git push -u origin master
 git remote add origin https://github.com/ahmedSohelcu/nativePro.git
 git push -u origin master
 ------------------------------------------------------
+
+
+1. Check your information
+---------------------------------------------
+git config --global --list
+
+2.Add your Git username and set your email
+---------------------------------------------
+git config --global user.name "YOUR_USERNAME"
+
+3. Then verify that you have the correct username:
+---------------------------------------------
+git config --global user.name
+
+
+4.To set your email address, type the following command:
+---------------------------------------------
+git config --global user.email "your_email_address@example.com"
+
+
+5.To verify that you entered your email correctly, type:
+---------------------------------------------
+git config --global user.email
+
+
+6. Check your information
+---------------------------------------------
+git config --global --list
