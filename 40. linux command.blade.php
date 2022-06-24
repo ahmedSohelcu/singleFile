eeddc0454201f6c14f6b85b072bd70db711e03f1
Ahmed ullah <46097797+ahmedSohelcu@users.noreply.github.com>
2022-06-24 10:34:02 +0600

    Update 40. linux command.blade.php

--- 40. linux command.blade.php
+++ 40. linux command.blade.php
@@ -1,5 +1,17 @@
 
+
+//-----------------------------------------
+      Basic command
+//-----------------------------------------
 1.Oper terminal =>  CTR + ALT + T
 2.Close terminal => CTR + D or CTR + SHIFT + W .
-3.Show username => whoami
+
+
+//-----------------------------------------
+      Linux command
+//-----------------------------------------
+1.Show username => whoami
+2.Show system and kernal => uname -a
+3.Repeat last command => !!
+4.Show environmen variable => env
 
