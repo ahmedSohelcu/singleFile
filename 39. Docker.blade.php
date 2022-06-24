1ad71709566244ffc8b9bfdf7244d0bb47ad8196
Ahmed ullah <46097797+ahmedSohelcu@users.noreply.github.com>
2022-06-25 00:31:18 +0600

    Update 39. Docker.blade.php

--- 39. Docker.blade.php
+++ 39. Docker.blade.php
@@ -2,4 +2,27 @@
 Docker install linux
 ----------------------------------
 sudo apt install docker.io
+docker --version
+
+----------------------------------
+To check Docker status
+----------------------------------
+sudo systemctl status docker 
+
+----------------------------------
+To run/enable Docker
+----------------------------------
+sudo systemctl enable --now docker
+
+----------------------------------
+To run
+----------------------------------
+sudo docker run hello-world
+
+
+----------------------------------
+all local images
+----------------------------------
+sudo docker images
+
 
