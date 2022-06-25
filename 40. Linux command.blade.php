f10e8f2a6a8b59b2fd6857e068de40a880111ef3
Ahmed ullah <46097797+ahmedSohelcu@users.noreply.github.com>
2022-06-25 11:15:41 +0600

    Update 40. Linux command.blade.php

--- 40. Linux command.blade.php
+++ 40. Linux command.blade.php
@@ -5,7 +5,11 @@
 //-----------------------------------------
 1.Oper terminal =>  CTR + ALT + T
 2.Close terminal => CTR + D or CTR + SHIFT + W .
-
+3.To know only the system name => uname
+4.To view your network hostname => uname -n
+5.To get information about kernel-version => uname -v
+6.To print your machine hardware name => uname -m
+7.All this information can be printed at once  => uname -a
 
 //-----------------------------------------
       Linux command
